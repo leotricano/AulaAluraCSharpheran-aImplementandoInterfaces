ae37d16777ca89e9fff1c2f74dde740f231320ec
Leonardo Tricano <leonardot@dime.com>
2023-02-02 12:42:15 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using bytebank_ADM.Funcionarios;
+
+
+Funcionario pedro = new Funcionario();
+pedro.Nome = "Pedro";
+pedro.Cpf = "110485596-10";
+pedro.Salario = 1000;
+
+Console.WriteLine(pedro.Nome);
+Console.WriteLine(pedro.GetBonificacao());
+
+Diretor roberta = new Diretor();
+roberta.Nome = "Roberta Silva";
+roberta.Cpf = "54354313";
+roberta.Salario = 5000;
+
+Console.WriteLine(roberta.Nome);
+Console.WriteLine(roberta.GetBonificacao());
